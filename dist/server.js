3e97e6ebbe5c548a5690ee7f3309e2e2e4d9ad92
Mateusz <mateusz@Kingas-MacBook-Air.local>
2017-07-24 13:39:40 +0200

    add new history middleware to server.js

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,14 @@
+var history = require('connect-history-api-fallback');
+
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 
 
 
-var app = express();
+app = express();
+
+app.use(history());
 app.use(serveStatic(__dirname));
 
 

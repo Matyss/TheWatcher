764e0426f123a4c4de74a6652080997e7b420539
Mateusz <mateusz@Kingas-MacBook-Air.local>
2017-07-26 14:15:33 +0200

    change routes for AWS S3

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,32 @@
-import Header from './components/shared/Header.vue';
 import Navbar from './components/shared/Navbar.vue';
+import Header from './components/shared/Header.vue';
 import Footer from './components/shared/Footer.vue';
-import Home from './components/Home.vue';
+
 import Landing from './components/Landing.vue';
+import Home from './components/Home.vue';
+import New from './components/New.vue';
+import Watched from './components/Watched.vue';
+import Quote from './components/Quote.vue';
 
+// Below setup is not working on AWS S3
 
-const New = resolve => {
-	require.ensure(['./components/New.vue'], () => {
-		resolve(require('./components/New.vue'));
-	}, 'new');
-}
+// const New = resolve => {
+// 	require.ensure(['./components/New.vue'], () => {
+// 		resolve(require('./components/New.vue'));
+// 	}, 'new');
+// }
 
-const Watched = resolve => {
-	require.ensure(['./components/Watched.vue'], () => {
-		resolve(require('./components/Watched.vue'));
-	}, 'watched');
-}
+// const Watched = resolve => {
+// 	require.ensure(['./components/Watched.vue'], () => {
+// 		resolve(require('./components/Watched.vue'));
+// 	}, 'watched');
+// }
 
-const Quote = resolve => {
-	require.ensure(['./components/Quote.vue'], () => {
-		resolve(require('./components/Quote.vue'));
-	}, 'quote');
-}
+// const Quote = resolve => {
+// 	require.ensure(['./components/Quote.vue'], () => {
+// 		resolve(require('./components/Quote.vue'));
+// 	}, 'quote');
+// }
 
 export const routes = [
 	{path: '/', components: {
